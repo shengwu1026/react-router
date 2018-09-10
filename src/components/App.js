5c689b1329f138503953d3ddd5b014fcc8a75501
wusheng <shengwu1026@gmail.com>
2018-09-10 23:32:01 +1000

    setup basic home layout

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './Home'
+import Navbar from './Navbar'
+import Players from './Players'
+import Teams from './Teams'
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navbar/>
+          
+          <Switch>
+            <Route path='/' exact component={Home}/>
+            <Route path='/players' component={Players}/>
+            <Route path='/teams' component={Teams}/>
+            <Route render={()=><h1 className='text-center'>404</h1>}/>
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default App
